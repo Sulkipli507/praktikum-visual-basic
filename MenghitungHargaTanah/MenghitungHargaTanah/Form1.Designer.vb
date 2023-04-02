b2075e7192a04cdff8c1f71aae13d8ab84257c11
Sulkipli507 <sulkiply507@gmail.com>
2023-04-02 20:56:34 +0800

    add menghitung harga tanah

---
+++ MenghitungHargaTanah/MenghitungHargaTanah/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.jarak = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.panjang = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lebar = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.luas = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.harga = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.kategori = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(173, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(241, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Jarak tanah Dengan Jalan Poros"
+        '
+        'jarak
+        '
+        Me.jarak.Location = New System.Drawing.Point(463, 64)
+        Me.jarak.Name = "jarak"
+        Me.jarak.Size = New System.Drawing.Size(100, 26)
+        Me.jarak.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(173, 114)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Panjang Tanah"
+        '
+        'panjang
+        '
+        Me.panjang.Location = New System.Drawing.Point(463, 108)
+        Me.panjang.Name = "panjang"
+        Me.panjang.Size = New System.Drawing.Size(100, 26)
+        Me.panjang.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(173, 161)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Lebar Tanah"
+        '
+        'lebar
+        '
+        Me.lebar.Location = New System.Drawing.Point(463, 155)
+        Me.lebar.Name = "lebar"
+        Me.lebar.Size = New System.Drawing.Size(100, 26)
+        Me.lebar.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(173, 235)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(93, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Luas Tanah"
+        '
+        'luas
+        '
+        Me.luas.Location = New System.Drawing.Point(463, 229)
+        Me.luas.Name = "luas"
+        Me.luas.Size = New System.Drawing.Size(100, 26)
+        Me.luas.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(173, 281)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(102, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Harga Tanah"
+        '
+        'harga
+        '
+        Me.harga.Location = New System.Drawing.Point(463, 275)
+        Me.harga.Name = "harga"
+        Me.harga.Size = New System.Drawing.Size(100, 26)
+        Me.harga.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(177, 333)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(116, 20)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Kategori Harga"
+        '
+        'kategori
+        '
+        Me.kategori.Location = New System.Drawing.Point(463, 326)
+        Me.kategori.Name = "kategori"
+        Me.kategori.Size = New System.Drawing.Size(100, 26)
+        Me.kategori.TabIndex = 11
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(315, 389)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 34)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Close"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(442, 389)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 34)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(570, 67)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(30, 20)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "km"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(569, 111)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(50, 20)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "meter"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(569, 155)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(50, 20)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "meter"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(427, 278)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(30, 20)
+        Me.Label10.TabIndex = 17
+        Me.Label10.Text = "Rp"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(570, 232)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(31, 20)
+        Me.Label11.TabIndex = 18
+        Me.Label11.Text = "m2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.kategori)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.harga)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.luas)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lebar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.panjang)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.jarak)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents jarak As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents panjang As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lebar As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents luas As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents harga As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents kategori As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+End Class
---
+++ MenghitungHargaTanah/MenghitungHargaTanah/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub lebar_TextChanged(sender As Object, e As EventArgs) Handles lebar.TextChanged
+
+        luas.Text = Val(panjang.Text) * Val(lebar.Text)
+
+
+        If Val(jarak.Text) <= 1 Then
+            harga.Text = 500
+        ElseIf Val(jarak.Text) > 1 And Val(jarak.Text) <= 5 Then
+            harga.Text = 450
+        ElseIf Val(jarak.Text) > 5 And Val(jarak.Text) <= 10 Then
+            harga.Text = 400
+        Else
+            harga.Text = 350
+        End If
+        harga.Text *= Val(luas.Text)
+
+
+        If Val(harga.Text) < 20000000 Then
+            kategori.Text = "Murah"
+        Else
+            kategori.Text = "Mahal"
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        panjang.Text = ""
+        lebar.Text = ""
+        luas.Text = ""
+        jarak.Text = ""
+        harga.Text = ""
+        kategori.Text = ""
+    End Sub
+End Class
